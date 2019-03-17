28426a44f458825592a90ee39a782a14442950d5
gillbalraj <balrajgill18@gmail.com>
2019-03-17 04:47:13 -0400

    added bootstrap and path module

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const router = express.Router();
-
-
-router.get('/', (req, res) => {
-    res.send("Inside route.js");
-});
-
+const path = require('path');
 
 // CORS Middleware
 app.use(cors());
 
-
 // API endpoint will appear in routes/routes.js 
 var routes = require("./routes/routes.js")(app);
+
+app.use(express.static(path.join(__dirname, 'stackpros-test/dist/stackpros-test')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'stackpros-test/dist/stackpros-test/index.html'));
+});
 
 var server = app.listen(3000, function() {
     console.log("Listening to port %s...", server.address().port);
